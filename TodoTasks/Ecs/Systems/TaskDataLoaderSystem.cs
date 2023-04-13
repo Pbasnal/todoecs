7aceda20b7dee99aad7f6cfa87af6358f6bc08e6
Pankaj Basnal <pankajbasnal17@gmail.com>
2023-04-13 22:55:52 +0530

    adding counter for number of entities loaded by loader

--- TodoTasks/Ecs/Systems/TaskDataLoaderSystem.cs
+++ TodoTasks/Ecs/Systems/TaskDataLoaderSystem.cs
@@ -1,9 +1,16 @@
+using Prometheus;
+using todorest;
+
 namespace TodoApp
 {
     public class TaskDataLoaderSystem : ISystem<TaskEntity>
     {
         private DataStore dataStore;
         private readonly ILogger _logger;
+
+        private static readonly Counter entityLoadCount = Metrics
+            .CreateCounter("entity_loaded_count", "Number of entities loaded by data loader.");
+        
 
         public TaskDataLoaderSystem()
         {
@@ -13,27 +20,33 @@ namespace TodoApp
 
         public void Execute(EntityArchetype<TaskEntity> entityArchetype)
         {
-            var requestPool = entityArchetype.GetComponentPool<TaskQuickViewRequestComponent>();
-            var quickViewTitles = entityArchetype.GetComponentPool<TaskQuickViewTitles>();
-
-            foreach (var entity in entityArchetype.GetActiveEntities())
+            using (TodoMetrics.MethodMetrics("TaskDataLoaderSystem"))
             {
-                ref var request = ref requestPool.GetElementAt(entity.Index);
-                // _logger.LogDebug($"Requested system for > {request.page} number of tasks: {request.numberOfTasks}");
-                var taskTitles = dataStore.GetTaskTitles(request.page, request.numberOfTasks);
+                var requestPool = entityArchetype.GetComponentPool<TaskQuickViewRequestComponent>();
+                var quickViewTitles = entityArchetype.GetComponentPool<TaskQuickViewTitles>();
 
-                string[] titles = new string[taskTitles.Count];
-                // _logger.LogDebug($"Number of tasks loaded from data store > {taskTitles.Count}");
-
-                int titleIndex = 0;
-                foreach (var title in taskTitles)
+                int numberOfEntitiesProcessed = 0;
+                foreach (var entity in entityArchetype.GetActiveEntities())
                 {
-                    titles[titleIndex++] = title.Value;
+                    numberOfEntitiesProcessed++;
+                    ref var request = ref requestPool.GetElementAt(entity.Index);
+                    // Console.WriteLine($"Requested system for > {request.page} number of tasks: {request.numberOfTasks}");
+                    var taskTitles = dataStore.GetTaskTitles(request.page, request.numberOfTasks);
+
+                    string[] titles = new string[taskTitles.Count];
+                    // Console.WriteLine($"Number of tasks loaded from data store > {taskTitles.Count}");
+
+                    int titleIndex = 0;
+                    foreach (var title in taskTitles)
+                    {
+                        titles[titleIndex++] = title.Value;
+                    }
+
+                    ref var quickViewTitle = ref quickViewTitles.GetElementAt(entity.Index);
+
+                    quickViewTitle.taskTitle = titles;
                 }
-
-                ref var quickViewTitle = ref quickViewTitles.GetElementAt(entity.Index);
-
-                quickViewTitle.taskTitle = titles;
+                entityLoadCount.Inc(numberOfEntitiesProcessed);
             }
         }
     }
